e4f3c30cf6a4e45d2134c1768da0b5aa1cf8645b
Renan Bernordi <altendorfme@gmail.com>
2024-12-05 13:11:56 -0300

    lista de user agents simplificada

--- app/data/user_agents.php
+++ app/data/user_agents.php
@@ -7,27 +7,7 @@
  * que podem ser utilizados para fazer requisições
  */
 return [
-    'Googlebot' => [
-        'user_agent' => 'Mozilla/5.0 (compatible; Googlebot/2.1; +https://www.google.com/bot.html)',
-        'headers' => [
-            'From' => 'googlebot(at)googlebot.com',
-            'X-Robots-Tag' => 'noindex'
-        ]
-    ],
-    'Bingbot' => [
-        'user_agent' => 'Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)',
-        'headers' => [
-            'From' => 'bingbot(at)microsoft.com',
-            'X-Robots-Tag' => 'noindex',
-            'X-MSEdge-Bot' => 'true'
-        ]
-    ],
-    'GPTBot' => [
-        'user_agent' => 'Mozilla/5.0 (compatible; GPTBot/1.0; +https://openai.com/gptbot)',
-        'headers' => [
-            'From' => 'gptbot(at)openai.com',
-            'X-Robots-Tag' => 'noindex',
-            'X-OpenAI-Bot' => 'true'
-        ]
-    ]
+    'Googlebot-News',
+    'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/W.X.Y.Z Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+    'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Chrome/W.X.Y.Z Safari/537.36'
 ];
